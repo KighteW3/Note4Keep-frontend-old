9b0c672f3b9f36711f8ddca363d1a8ea3abde48a
Kighte <kighte04@gmail.com>
2023-09-06 17:21:04 +0200

    host.ts host_f change

--- src/assets/host.ts
+++ src/assets/host.ts
@@ -1,4 +1,4 @@
 export const hostB = "note4keep-backend.onrender.com";
-export const hostF = import.meta.env.VITE_HOST_F;
+export const hostF = `${import.meta.env.VITE_HOST_F}`;
 export const portB = "5722";
 export const portF = "5173";

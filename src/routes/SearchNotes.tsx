69c521fefad9d8e2eb22873c1ea744f5ed3a7141
kighteW3 <kighte04@gmail.com>
2023-10-09 20:38:06 +0200

    Now when a note search is not found, it will print it

--- src/routes/SearchNotes.tsx
+++ src/routes/SearchNotes.tsx
@@ -125,7 +125,7 @@ export default function SearchNotes() {
               );
             })
           ) : (
-            <></>
+            <>Not found...</>
           )}
         </div>
         <NotePageNav
